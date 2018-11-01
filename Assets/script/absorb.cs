b7ee32a5174a61c3180f24dca6ef63cbe6bd8313
FR <2717119855@qq.com>
2018-11-01 14:05:06 +0800

    new

---
+++ Assets/script/absorb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class absorb : MonoBehaviour {
+    public bool isImgOn;
+    public Image img;
+    public int color;
+    public GameObject player;
+    status Status;
+    // Use this for initialization
+    void Start () {
+
+        img.enabled = false;
+        isImgOn = false;
+        Status = player.GetComponent<status>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		//Check switch if button down
+        //absorb
+	}
+    
+    void OnTriggerEnter(Collider other)
+    {
+        print("trigger");
+        //Check Tag
+        other.tag = "Player";
+        if (isImgOn == false)
+        {
+            img.enabled = true;
+            isImgOn = true;
+        }
+            //If Tag player pop-up menu(UI)
+            if (Input.GetKey("j")) {
+                print("ABSORB!!");
+            status.absorbColor(color);
+
+        }
+            //Switch
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isImgOn == true)
+        {
+
+            img.enabled = false;
+            isImgOn = false;
+        }
+    }
+}
---
+++ Assets/script/status.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class status : MonoBehaviour {
+    static int[] slots = {0,0,0};
+    static //white:0 R:1   Y:2   B:3                                                 
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int n = 0; n < 3; n++)
+        {
+            switch (slots[n]) {
+                case 1:
+                        break;
+                case 2:
+                    
+                        break;
+                case 3:
+                        break;
+                case 0:
+                        break;
+
+            }
+
+        }
+    }
+
+    public static void absorbColor(int color) {
+        for (int n = 0; n < 3; n++) {
+            if (slots[n] == 0) {
+                slots[n] = color;
+            }
+        }
+    }
+}
