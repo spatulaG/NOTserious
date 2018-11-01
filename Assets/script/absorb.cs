214a62a57015f36cea7d22002de02d5cd153f4fc
FR <2717119855@qq.com>
2018-11-01 16:00:32 +0800

    status + absorb

--- Assets/script/absorb.cs
+++ Assets/script/absorb.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,36 +8,77 @@ public class absorb : MonoBehaviour {
     public Image img;
     public int color;
     public GameObject player;
-    status Status;
+    private Renderer rend;
     // Use this for initialization
     void Start () {
 
         img.enabled = false;
         isImgOn = false;
-        Status = player.GetComponent<status>();
+        rend = player.GetComponent<Renderer>();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		//Check switch if button down
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check switch if button down
+        attack();
         //absorb
-	}
-    
+        absorbColor();
+        //If Tag player pop-up menu(UI)
+        
+    }
+
+    private void attack()
+    {
+
+    }
+
+    private void absorbColor()
+    {
+        if (Input.GetKey("j") && isImgOn)
+        {
+            print("ABSORB!!");
+            switch (color)
+            {
+                case 0:
+                    rend.material.SetColor("_Color", Color.white);
+                    break;
+                case 1:
+                    rend.material.SetColor("_Color", Color.red);
+                    break;
+                case 2:
+                    rend.material.SetColor("_Color", Color.yellow);
+                    break;
+                case 3:
+                    rend.material.SetColor("_Color", Color.blue);
+                    break;
+                case 4:
+                    rend.material.SetColor("_Color", Color.green);
+                    break;
+                case 5:
+                    rend.material.SetColor("_Color", Color.black);
+                    break;
+                case 6:
+                    rend.material.SetColor("_Color", Color.gray);
+                    break;
+
+            }
+        }
+    }
+
     void OnTriggerEnter(Collider other)
     {
         print("trigger");
         //Check Tag
-        other.tag = "Player";
-        if (isImgOn == false)
-        {
-            img.enabled = true;
-            isImgOn = true;
-        }
-            //If Tag player pop-up menu(UI)
-            if (Input.GetKey("j")) {
-                print("ABSORB!!");
-            status.absorbColor(color);
-
+        if(other.tag == "Player") {
+            print("player!");
+            if (isImgOn == false)
+            {
+                img.enabled = true;
+                isImgOn = true;
+            }
+            
+            
         }
             //Switch
     }
@@ -48,6 +90,7 @@ public class absorb : MonoBehaviour {
 
             img.enabled = false;
             isImgOn = false;
+            
         }
     }
 }
--- Assets/script/status.cs
+++ Assets/script/status.cs
@@ -1,39 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class status : MonoBehaviour {
-    static int[] slots = {0,0,0};
-    static //white:0 R:1   Y:2   B:3                                                 
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        for (int n = 0; n < 3; n++)
-        {
-            switch (slots[n]) {
-                case 1:
-                        break;
-                case 2:
-                    
-                        break;
-                case 3:
-                        break;
-                case 0:
-                        break;
+    public int HP = 3;
+    public int[] slot = {0,0,0};
 
-            }
-
-        }
-    }
-
-    public static void absorbColor(int color) {
-        for (int n = 0; n < 3; n++) {
-            if (slots[n] == 0) {
-                slots[n] = color;
-            }
-        }
-    }
 }
