8e5c4ab5f90b4accffe89d038624c5bec42485b9
FR <2717119855@qq.com>
2018-11-02 12:53:47 +0800

    bullet !!!

--- Assets/Resources/Scripts/Hero.cs
+++ Assets/Resources/Scripts/Hero.cs
@@ -19,15 +19,18 @@ public class Hero : MonoBehaviour {
     
     void shoot()
     {
-        if(facingDirection == FacingDirection.FacingLeft)
+        //Resources.Load("Prefabs/bullet")
+        if (facingDirection == FacingDirection.FacingLeft)
         {
-            bullet = Instantiate(Resources.Load("Prefabs/bullet"), hero.transform.position - new Vector3(hero.GetComponent<Collider2D>().bounds.size.x / 2, 0, 0), Quaternion.identity) as GameObject;
+            bullet = Instantiate(bullet, hero.transform.position - new Vector3(hero.GetComponent<Collider2D>().bounds.size.x / 2, 0, 0), Quaternion.identity) as GameObject;
             bullet.GetComponent<Rigidbody2D>().AddForce(Vector3.left,ForceMode2D.Impulse);
+            bullet.tag = "bullet";
         }
         else
         {
             bullet = Instantiate(Resources.Load("Prefabs/bullet"), hero.transform.position + new Vector3(hero.GetComponent<Collider2D>().bounds.size.x / 2, 0, 0), Quaternion.identity) as GameObject;
             bullet.GetComponent<Rigidbody2D>().AddForce(Vector3.right, ForceMode2D.Impulse);
+            bullet.tag = "bullet";
         }
         StartCoroutine(DestroyBullet(1.0f,bullet));
     }
--- Assets/script/status.cs
+++ Assets/script/status.cs
@@ -7,5 +7,12 @@ using UnityEngine.UI;
 public class status : MonoBehaviour {
     public int HP = 3;
     public int[] slot = {0,0,0};
+    public GameObject bullet;
+    private void Update()
+    {
+        GameObject[] bullet = GameObject.FindGameObjectsWithTag("bullet");
+        for(int n=0;n<bullet.Length;n++)
+            bullet[n].GetComponent<SpriteRenderer>().color = this.GetComponent<SpriteRenderer>().color;
+    }
 
 }
