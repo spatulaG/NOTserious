88a59a94859f5335f0cf71686fe27c5c37c1f2a0
FR <2717119855@qq.com>
2018-11-02 17:08:34 +0800

    oppo color destroy enemy!!!

---
+++ Assets/script/UI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+    public GameObject menu;
+	// Use this for initialization
+	void Start () {
+        menu.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
